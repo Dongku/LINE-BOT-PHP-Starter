f910638fb67281a5d257736014242ca2d90ac263
Dongku <dong_t49@hotmail.com>
2017-04-09 15:40:12 +0700

    Update dongkubot.php

--- dongkubot.php
+++ dongkubot.php
@@ -4,15 +4,14 @@ $proxy = 'http://fixie:dktIJfZt6pqxvZ1@velodrome.usefixie.com:80';
 $proxyauth = 'dong_t49@hotmail.com:Kikko664629';
 $access_token = 'g4wgtByEcy99M5Zx8bw4fIJJtqi0b/Nwq1mcI4HPXVK2SrFrrMfz/J4gF44eOeNozHuCPQjkuDB8dfk58vFh9bRlk252NZmcNh5n6VPVnT5o8HXIFb9e2H++oIm3t+Bn90HESDRCM4smmZhrd7pppwdB04t89/1O/w1cDnyilFU=';
 
- 
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
 $strUrl = "https://api.line.me/v2/bot/message/reply";
  
 $arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$arrHeader[] = 'Content-Type: application/json';
+$arrHeader[] = 'Authorization: Bearer' .$strAccessToken;
  
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
@@ -45,7 +44,6 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "พูดอะไร ฉันไม่เข้าใจคำสั่ง";
 }
  
- 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -54,7 +52,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close ($ch);
- 
+
+echo "BOT OK";
 ?>
