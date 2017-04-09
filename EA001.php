2961d077b309297378c5d6e992e264f6cde77da9
Dongku <dong_t49@hotmail.com>
2017-04-09 14:30:47 +0700

    Update EA001.php

--- EA001.php
+++ EA001.php
@@ -2,7 +2,7 @@
 $url = 'https://api.pushbullet.com/v2/users/me';
 $access_token = 'o.em63fJtsdieIDm46GxFJBasM6udgSvzT';
 
-$headers = array('Access-Token: Bearer ' . $access_token);
+$headers = array('Access-Token:'. $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
