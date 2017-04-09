3d6b6c575fbb7ec1efd0e350cedd56625a74431f
Dongku <dong_t49@hotmail.com>
2017-04-09 14:19:18 +0700

    Update EA001.php

--- EA001.php
+++ EA001.php
@@ -1,6 +1,6 @@
 <?php
-$proxy = 'http://fixie:dktIJfZt6pqxvZ1@velodrome.usefixie.com:80';
-$proxyauth = 'dong_t49@hotmail.com:Kikko664629';
+//$proxy = 'http://fixie:dktIJfZt6pqxvZ1@velodrome.usefixie.com:80';
+//$proxyauth = 'dong_t49@hotmail.com:Kikko664629';
 
 $access_token = 'o.em63fJtsdieIDm46GxFJBasM6udgSvzT';
 $url = 'https://api.pushbullet.com/v2/pushes';
@@ -9,14 +9,14 @@ $url = 'https://api.pushbullet.com/v2/pushes';
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 //$arrHeader[] = "Authorization: Bearer {$access_token}";
-$arrHeader[] = "Access-Token: {$access_token}";
+$arrHeader[] = "Access-Token: $access_token";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrheaders);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
