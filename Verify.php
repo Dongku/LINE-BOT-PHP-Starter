7269fa47b1d911faeff185da6994b009880a47a5
Dongku <dong_t49@hotmail.com>
2017-04-09 13:37:09 +0700

    Update Verify.php

--- Verify.php
+++ Verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = '54.173.229.200:24';
+$proxyauth = 'dong_t49@hotmail.com:Kikko664629';
+
 $access_token = 'g4wgtByEcy99M5Zx8bw4fIJJtqi0b/Nwq1mcI4HPXVK2SrFrrMfz/J4gF44eOeNozHuCPQjkuDB8dfk58vFh9bRlk252NZmcNh5n6VPVnT5o8HXIFb9e2H++oIm3t+Bn90HESDRCM4smmZhrd7pppwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
