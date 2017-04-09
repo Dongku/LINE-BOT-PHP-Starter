b830a721de6b5f30b3651c1aa12643322bec8332
Dongku <dong_t49@hotmail.com>
2017-04-09 13:50:55 +0700

    Update EA001.php

--- EA001.php
+++ EA001.php
@@ -11,7 +11,7 @@ $arrHeader[] = "Authorization: Bearer {$access_token}";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrheaders);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_PROXY, $proxy);
 curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
