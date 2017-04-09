2bf488581075b866f5c792ec837c7350e501aef4
Dongku <dong_t49@hotmail.com>
2017-04-09 14:02:30 +0700

    Update EA001.php

--- EA001.php
+++ EA001.php
@@ -3,7 +3,9 @@ $proxy = 'http://fixie:dktIJfZt6pqxvZ1@velodrome.usefixie.com:80';
 $proxyauth = 'dong_t49@hotmail.com:Kikko664629';
 
 $access_token = 'o.ipkOlaHhmMuRCp4wNM9Xoo0jyDoUO22w';
-$url = 'https://api.pushbullet.com/v2/pushes';
+//$url = 'https://api.pushbullet.com/v2/pushes';
+$url = 'https://api.pushbullet.com/v2/users/me';
+
 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
