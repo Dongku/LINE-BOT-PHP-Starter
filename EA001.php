96b3049460334b417575237841c550c06246669b
Dongku <dong_t49@hotmail.com>
2017-04-09 14:27:19 +0700

    Update EA001.php

--- EA001.php
+++ EA001.php
@@ -1,22 +1,14 @@
 <?php
-//$proxy = 'http://fixie:dktIJfZt6pqxvZ1@velodrome.usefixie.com:80';
-//$proxyauth = 'dong_t49@hotmail.com:Kikko664629';
-
+$url = 'https://api.pushbullet.com/v2/users/me';
 $access_token = 'o.em63fJtsdieIDm46GxFJBasM6udgSvzT';
-$url = 'https://api.pushbullet.com/v2/pushes';
-//$url = 'https://api.pushbullet.com/v2/users/me';
 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-//$arrHeader[] = "Authorization: Bearer {$access_token}";
-$arrHeader[] = "Access-Token: $access_token";
+$headers = array('Access-Token: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrheaders);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-//curl_setopt($ch, CURLOPT_PROXY, $proxy);
-//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
+
 echo $result;
