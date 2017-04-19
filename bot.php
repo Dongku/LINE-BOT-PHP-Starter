6a30ede8007c35daec4b09df1bfe2a2cf0653c23
Dongku <dong_t49@hotmail.com>
2017-04-20 02:11:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,13 +27,12 @@ if (!is_null($events['events'])) {
 			];
     			echo "True";
 			}
-			else if {
+			else {
 			$messages = [
 				'type' => 'text',
 				'text' => 'ไปหาเองไป๊..'
 			];
 			}
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
