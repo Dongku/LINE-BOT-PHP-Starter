f3a8420cb8454df46149179b925f6658e7a5b579
Dongku <dong_t49@hotmail.com>
2018-04-07 00:02:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
     			echo "True";
 			}
 			else if (stripos($text, "group") !== false) {
-			$text3 = $event['source']['roomId'];
+			$text3 = $event['source']['groupId'];
 			$messages = [
 				'type' => 'text',
 				'text' => 'GROUP ID='.$text3
