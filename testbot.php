7759c6a9c69cc94d30329745994bddb985150820
Dongku <dong_t49@hotmail.com>
2018-04-16 23:25:53 +0700

    Update testbot.php

--- testbot.php
+++ testbot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+//include ('line-bot-api/php/line-bot.php');
 
 $channelSecret = 'ed498d7eb3f9ff4a0419161e80077664';
 $access_token  = '7ZOo8dAN3AJre+UNo8Vo7fiKmagJ55ShMcLXf/f/qfH1fHzImu3dB+KiCFiZ7QK/mU/hjt8rev/nxQL0uxEZW5ztW5Gn+14C/U1XChNgHHypz6LpdbJgYK8U/hYv+gYAn0wdjEx7dvjKxLvinQCabwdB04t89/1O/w1cDnyilFU=';
@@ -23,4 +23,5 @@ if (!empty($bot->isEvents)) {
 	exit();
 
 }
+
 echo "Verify-OK";
