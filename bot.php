312cd8acf08dcfdaac593fdf012575ddbd59c082
Dongku <dong_t49@hotmail.com>
2018-04-16 21:03:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,8 +19,8 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			
 			// Build message to reply back
-			$text = "Hello World!";
-			/*
+			//$text = "Hello World!";
+			
 			if (stripos($text, "Siamchart_") !== false) {
 			$text1 = str_replace("Siamchart_", "", $text);
 			$messages = [
@@ -79,14 +79,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => 'Test Test'
 			];
-			}
-			*/
-			
-			$messages = [
-				'type' => 'text',
-				'text' => 'Test Test'
-			];
-			
+			}			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
